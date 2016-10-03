2116403c155716c8dfd74faf6c195296c8a71e31
Frank Staude <frank@staude.net>
2016-10-03 12:09:45 +0200

    init

---
+++ stream-rocket.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: AP Stream to Rocket
+ * Plugin URI: https://wordpress.org/plugins/ap-stream-to-rocket/
+ * Description: Send Stream logs to rocket.chat.
+ * Author: f.staude
+ * Version: 0.0.1
+ * Author URI: https://staude.net/
+ * Text Domain: ap-stream-to-rocket
+ * Domain Path: /languages
+ * GitHub Plugin URI: https://github.com/adminpress/ap-stream-to-rocket
+ * GitHub Branch: master
+ */
+
+require_once dirname( __FILE__ ) . '/inc/class-stream-rocket-api.php';
+
+function register_stream_rocket() {
+	$stream_rocket = new Stream_Rocket_API();
+}
+add_action( 'init', 'register_stream_rocket' );
+
